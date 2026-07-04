3b5ca84dd92e3c54f0cd57309dfe66ee009cf4ee
Zerobase42 <malbolgeisfun@naver.com>
2026-07-04 21:36:10 +0900

    ntt smid - 수정 필요(300ms / 210ms)

--- ntt_faster_two.c
+++ ntt_faster_two.c
@@ -1,5 +1,5 @@
 #define DEBUG 0
-#define BARRET 0 // useless
+#define BARRET 1
 #define SMID 1
 //설정
 #ifndef _MEMORY_H
@@ -38,26 +38,14 @@ const
 #endif
 u32 w1=3,w2=3,mod1=998244353,mod2=1004535809,inv_mod1=669690699,k=93;
 #if BARRET==1
-#ifdef __cplusplus
-constexpr
-#else
-const
-#endif
-u64 x1=((u128(1)<<k)+mod1-1)/mod1,x2=((u128(1)<<k)+mod2-1)/mod2;
+static const u64 mu1=(u64)(((u128)1<<64)/mod1);
+static const u64 mu2=(u64)(((u128)1<<64)/mod2);
+static const u32 mu1_lo=(u32)mu1, mu1_hi=(u32)(mu1>>32);
+static const u32 mu2_lo=(u32)mu2, mu2_hi=(u32)(mu2>>32);
 #endif
 static u32 root[MAX>>1],rev[MAX],lastRev,a[MAX],b[MAX],c1[MAX],c2[MAX],A[MAX],B[MAX],na,nb;
 static u32 C[MAX];
 static char io_buf[(NUMLEN<<1)+5],tmp[20];
-#if BARRET==1
-static __inline u32 modular1(u128 x){
-    u32 ret=((u128)x*x1)>>k;
-    return x-ret*mod1;
-}
-static __inline u32 modular2(u128 x){
-    u32 ret=((u128)x*x2)>>k;
-    return x-ret*mod2;
-}
-#endif
 #ifdef SMID
 static __inline void parse_blocks_simd(char*buf,int hi,int lo,u32*out,int*idx){
     const __m256i ten=_mm256_set1_epi32(10);
@@ -79,28 +67,61 @@ static __inline void parse_blocks_simd(char*buf,int hi,int lo,u32*out,int*idx){
         for(int b=0;b<8;b++)out[(*idx)++]=r[b];
         i-=8*DIG;
     }
-    for(;i>lo;i-=DIG){                    
+    for(;i>lo;i-=DIG){
         int l=max(lo,i-DIG);
         u32 r=0;
         for(int j=l;j<i;j++)r=r*10+(buf[j]-'0');
         out[(*idx)++]=r;
     }
 }
-// barret to shorter code can!
+#if BARRET==1
+static __inline __m128i mulmod_simd_epu32x4(__m128i av,__m128i bv,u32 mod,u32 mu_lo,u32 mu_hi){
+    __m256i awide=_mm256_cvtepu32_epi64(av);
+    __m256i bwide=_mm256_cvtepu32_epi64(bv);
+    __m256i prod=_mm256_mul_epu32(awide,bwide);
+
+    __m256i mask32=_mm256_set1_epi64x(0xFFFFFFFFULL);
+    __m256i xl=_mm256_and_si256(prod,mask32);
+    __m256i xh=_mm256_srli_epi64(prod,32);
+
+    __m256i ylv=_mm256_set1_epi64x(mu_lo), yhv=_mm256_set1_epi64x(mu_hi);
+    __m256i p0=_mm256_mul_epu32(xl,ylv);
+    __m256i p1=_mm256_mul_epu32(xl,yhv);
+    __m256i p2=_mm256_mul_epu32(xh,ylv);
+    __m256i p3=_mm256_mul_epu32(xh,yhv);
+
+    __m256i mid=_mm256_add_epi64(_mm256_add_epi64(p1,p2),_mm256_srli_epi64(p0,32));
+    __m256i q  =_mm256_add_epi64(p3,_mm256_srli_epi64(mid,32));
+
+    __m256i modvec=_mm256_set1_epi64x(mod);
+    __m256i qmod=_mm256_mul_epu32(q,modvec);
+    __m256i r=_mm256_sub_epi64(prod,qmod);
+
+    for(int rep=0;rep<3;rep++){
+        __m256i ge=_mm256_cmpgt_epi64(r,_mm256_sub_epi64(modvec,_mm256_set1_epi64x(1)));
+        r=_mm256_sub_epi64(r,_mm256_and_si256(ge,modvec));
+    }
+    const __m256i shuf_idx=_mm256_setr_epi32(0,2,4,6,1,3,5,7);
+    __m256i packed=_mm256_permutevar8x32_epi32(r,shuf_idx);
+    return _mm256_castsi256_si128(packed);
+}
+#define MULMOD1(av,bv) mulmod_simd_epu32x4(av,bv,mod1,mu1_lo,mu1_hi)
+#define MULMOD2(av,bv) mulmod_simd_epu32x4(av,bv,mod2,mu2_lo,mu2_hi)
+#else
 static __inline __m128i mulmod_epu32x4(__m128i av,__m128i bv,u32 mod){
     const __m256d inv_modd=_mm256_set1_pd(1.0/(double)mod);
     const __m256i modvec64=_mm256_set1_epi64x(mod);
-    __m256d da=_mm256_cvtepi32_pd(av);        
+    __m256d da=_mm256_cvtepi32_pd(av);
     __m256d db=_mm256_cvtepi32_pd(bv);
-    __m256d prod=_mm256_mul_pd(da,db);        
+    __m256d prod=_mm256_mul_pd(da,db);
     __m256d q_d=_mm256_floor_pd(_mm256_mul_pd(prod,inv_modd));
-    __m128i q=_mm256_cvttpd_epi32(q_d);      
+    __m128i q=_mm256_cvttpd_epi32(q_d);
     __m256i awide=_mm256_cvtepu32_epi64(av);
     __m256i bwide=_mm256_cvtepu32_epi64(bv);
     __m256i prod_exact=_mm256_mul_epu32(awide,bwide);
     __m256i qwide=_mm256_cvtepu32_epi64(q);
-    __m256i qmod=_mm256_mul_epu32(qwide,modvec64);    
-    __m256i r=_mm256_sub_epi64(prod_exact,qmod);     
+    __m256i qmod=_mm256_mul_epu32(qwide,modvec64);
+    __m256i r=_mm256_sub_epi64(prod_exact,qmod);
     for(int rep=0;rep<2;rep++){
         __m256i ge=_mm256_cmpgt_epi64(r,_mm256_sub_epi64(modvec64,_mm256_set1_epi64x(1)));
         r=_mm256_sub_epi64(r,_mm256_and_si256(ge,modvec64));
@@ -113,14 +134,17 @@ static __inline __m128i mulmod_epu32x4(__m128i av,__m128i bv,u32 mod){
     __m256i packed=_mm256_permutevar8x32_epi32(r,shuf_idx);
     return _mm256_castsi256_si128(packed);
 }
+#define MULMOD1(av,bv) mulmod_epu32x4(av,bv,mod1)
+#define MULMOD2(av,bv) mulmod_epu32x4(av,bv,mod2)
+#endif
 static __inline __m256i div10_epu32(__m256i x,__m256i*rem){
     const __m256i magic=_mm256_set1_epi32((int)0xCCCCCCCD);
     const __m256i ten=_mm256_set1_epi32(10);
-    __m256i odd=_mm256_srli_epi64(x,32);                    
+    __m256i odd=_mm256_srli_epi64(x,32);
     __m256i pe=_mm256_srli_epi64(_mm256_mul_epu32(x,magic),35); 
     __m256i po=_mm256_srli_epi64(_mm256_mul_epu32(odd,magic),35);
     __m256i q=_mm256_or_si256(pe,_mm256_slli_epi64(po,32));
-    *rem=_mm256_sub_epi32(x,_mm256_mullo_epi32(q,ten));     
+    *rem=_mm256_sub_epi32(x,_mm256_mullo_epi32(q,ten));
     return q;
 }
 #endif
@@ -169,15 +193,42 @@ static __inline void ntt1(u32*f,int n){
     }
     int step=n>>1;
     for(int len=2;len<=n;len<<=1){
+        int half=len>>1;
         for(int j=0;j<n;j+=len){
-            for(int k=0;k<(len>>1);k++){
-                u32 u=f[j+k],v=(u64)f[j+k+(len>>1)]*root[step*k]%mod1;
+            int k=0;
+#if SMID==1
+            const __m128i stepvec=_mm_set1_epi32(step);
+            const __m128i idxbase=_mm_setr_epi32(0,1,2,3);
+            const __m128i modvec=_mm_set1_epi32(mod1);
+            const __m128i one=_mm_set1_epi32(1);
+            for(;k+4<=half;k+=4){
+                __m128i kvec=_mm_add_epi32(_mm_set1_epi32(k),idxbase);
+                __m128i rootidx=_mm_mullo_epi32(kvec,stepvec);
+                __m128i rootv=_mm_i32gather_epi32((const int*)root,rootidx,4);
+                __m128i uvec=_mm_loadu_si128((__m128i*)&f[j+k]);
+                __m128i vraw=_mm_loadu_si128((__m128i*)&f[j+k+half]);
+                __m128i vvec=MULMOD1(vraw,rootv);
+
+                __m128i sum=_mm_add_epi32(uvec,vvec);
+                __m128i ge1=_mm_cmpgt_epi32(sum,_mm_sub_epi32(modvec,one));
+                sum=_mm_sub_epi32(sum,_mm_and_si128(ge1,modvec));
+
+                __m128i diff=_mm_add_epi32(_mm_sub_epi32(uvec,vvec),modvec);
+                __m128i ge2=_mm_cmpgt_epi32(diff,_mm_sub_epi32(modvec,one));
+                diff=_mm_sub_epi32(diff,_mm_and_si128(ge2,modvec));
+
+                _mm_storeu_si128((__m128i*)&f[j+k],sum);
+                _mm_storeu_si128((__m128i*)&f[j+k+half],diff);
+            }
+#endif
+            for(;k<half;k++){
+                u32 u=f[j+k],v=(u64)f[j+k+half]*root[step*k]%mod1;
                 u+=v;
                 v=(u64)u+mod1-((u64)v<<1);
                 if(u>=mod1)u-=mod1;
                 if(v>=mod1)v-=mod1;
                 f[j+k]=u;
-                f[j+k+(len>>1)]=v;
+                f[j+k+half]=v;
             }
         }
         step>>=1;
@@ -194,15 +245,42 @@ static __inline void ntt2(u32*f,int n){
     }
     int step=n>>1;
     for(int len=2;len<=n;len<<=1){
+        int half=len>>1;
         for(int j=0;j<n;j+=len){
-            for(int k=0;k<(len>>1);k++){
-                u32 u=f[j+k],v=(u64)f[j+k+(len>>1)]*root[step*k]%mod2;
+            int k=0;
+#if SMID==1
+            const __m128i stepvec=_mm_set1_epi32(step);
+            const __m128i idxbase=_mm_setr_epi32(0,1,2,3);
+            const __m128i modvec=_mm_set1_epi32(mod2);
+            const __m128i one=_mm_set1_epi32(1);
+            for(;k+4<=half;k+=4){
+                __m128i kvec=_mm_add_epi32(_mm_set1_epi32(k),idxbase);
+                __m128i rootidx=_mm_mullo_epi32(kvec,stepvec);
+                __m128i rootv=_mm_i32gather_epi32((const int*)root,rootidx,4);
+                __m128i uvec=_mm_loadu_si128((__m128i*)&f[j+k]);
+                __m128i vraw=_mm_loadu_si128((__m128i*)&f[j+k+half]);
+                __m128i vvec=MULMOD2(vraw,rootv);
+
+                __m128i sum=_mm_add_epi32(uvec,vvec);
+                __m128i ge1=_mm_cmpgt_epi32(sum,_mm_sub_epi32(modvec,one));
+                sum=_mm_sub_epi32(sum,_mm_and_si128(ge1,modvec));
+
+                __m128i diff=_mm_add_epi32(_mm_sub_epi32(uvec,vvec),modvec);
+                __m128i ge2=_mm_cmpgt_epi32(diff,_mm_sub_epi32(modvec,one));
+                diff=_mm_sub_epi32(diff,_mm_and_si128(ge2,modvec));
+
+                _mm_storeu_si128((__m128i*)&f[j+k],sum);
+                _mm_storeu_si128((__m128i*)&f[j+k+half],diff);
+            }
+#endif
+            for(;k<half;k++){
+                u32 u=f[j+k],v=(u64)f[j+k+half]*root[step*k]%mod2;
                 u+=v;
                 v=(u64)u+mod2-(v<<1);
                 if(u>=mod2)u-=mod2;
                 if(v>=mod2)v-=mod2;
                 f[j+k]=u;
-                f[j+k+(len>>1)]=v;
+                f[j+k+half]=v;
             }
         }
         step>>=1;
@@ -229,7 +307,7 @@ static __inline void conv1(u32*c,int n){
     for(;i+4<=n;i+=4){
         __m128i av=_mm_loadu_si128((__m128i*)&a[i]);
         __m128i bv=_mm_loadu_si128((__m128i*)&b[i]);
-        __m128i cv=mulmod_epu32x4(av,bv,mod1);
+        __m128i cv=MULMOD1(av,bv);
         _mm_storeu_si128((__m128i*)&c[i],cv);
     }
     for(;i<n;i++) c[i]=(u64)a[i]*b[i]%mod1;
@@ -268,7 +346,7 @@ static __inline void conv2(u32*c,int n){
     for(;i+4<=n;i+=4){
         __m128i av=_mm_loadu_si128((__m128i*)&a[i]);
         __m128i bv=_mm_loadu_si128((__m128i*)&b[i]);
-        __m128i cv=mulmod_epu32x4(av,bv,mod2);
+        __m128i cv=MULMOD2(av,bv);
         _mm_storeu_si128((__m128i*)&c[i],cv);
     }
     for(;i<n;i++) c[i]=(u64)a[i]*b[i]%mod2;
